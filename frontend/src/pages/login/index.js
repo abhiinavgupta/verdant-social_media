3488e5c07cb668e9050a9d19c88bf9f3f4e5d585
abhiinavgupta <121946359+abhiinavgupta@users.noreply.github.com>
2023-05-07 23:58:06 +0530

    register page
    responsive

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -12,7 +12,7 @@ export default function Login() {
     const[login, setLogin]= useState(loginInfos);
     const{email, password}= login ;
 
-    console.log(login);
+    
     const handleLoginChange = (e) => {
         const {name, value} = e.target;
         setLogin({ ...login, [name]: value});
@@ -45,12 +45,12 @@ export default function Login() {
                 {(formik) => (
                     <Form>
                     <LoginInput type="text" name="email" placeholder="email address or phone number" onChange={handleLoginChange} />
-                    <LoginInput type="password" name="password" placeholder="password" onChange={handleLoginChange} />
+                    <LoginInput type="password" name="password" placeholder="password" onChange={handleLoginChange} bottom />
                     <button type="submit" className="blue_btn"> Log In</button>
                     </Form>
                 )}
                  </Formik>
-                 <Link to="/forgot" className="forget_password">Forgotten Password ?</Link>
+                 <Link to="/forgot" className="forget_password">Forgotten Password?</Link>
                  <div className="sign_splitter"></div>
                  <div className="blue_btn open_signup">Create Account</div>
             </div>
@@ -61,6 +61,65 @@ export default function Login() {
         </div>
     </div>
         <div className="register"></div>
+        <footer className="login_footer">
+            <div className="login_footer_wrap">
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" > english </Link>
+                <Link to="/" className="footer_square" >
+                    <i className="plus_icon"></i>
+                </Link>
+
+            </div>
+            <div className="footer_splitter"></div>
+            <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+
+<Link to="/">Log in</Link>
+<Link to="/">Watch </Link>
+<Link to="/">Places</Link>
+<Link to="/">Marketplace</Link>
+<Link to="/">  Oculus </Link>
+
+<Link to="/">  Portal</Link>
+
+<Link to="/">  Instagram</Link>
+
+<Link to="/">  Bulletin</Link>
+
+<Link to="/">  Local</Link>
+
+<Link to="/">  Fundraisers </Link>
+
+<Link to="/">  Services</Link> 
+<Link to="/">  Voting Information Centre</Link>
+
+<Link to="/">  Groups </Link>
+
+<Link to="/">  About</Link>
+
+<Link to="/">  Create ad</Link>
+
+<Link to="/">  Create Page</Link>
+
+<Link to="/">  Developers </Link>
+<Link to="/">
+AdChoices
+    <i className="adChoices_icon"></i>
+</Link>
+<Link to="/">  Careers</Link>
+
+<Link to="/">  Privacy</Link> <Link to="/">  Cookies</Link>
+
+            </div>
+<div className="login_footer_wrap" style={{fontSize:"12px", marginTop:"10px"}}> Abhinav Gupta Ⓒ 2023</div>
+        </footer>
     </div>
 
     </div>
