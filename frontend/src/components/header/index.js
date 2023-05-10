1e51a70789a9feac68920df3532a65c1f691cf25
abhiinavgupta <121946359+abhiinavgupta@users.noreply.github.com>
2023-05-11 00:28:51 +0530

    added some more responsiveness
    responsiveness

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -78,7 +78,7 @@ export default function Header() {
             <img src={user?.picture} alt="" />
             <span>{user?.first_name}</span>
         </Link>
-        <div className="circle_icon hover1"
+        <div className={`circle_icon hover1 ${showAllMenu && "active_header"}`}
         ref={allMenu}
          >
         <div onClick={()=>{
@@ -97,7 +97,7 @@ export default function Header() {
             <Notifications />
             <div className="right_notification">5</div>
         </div>
-        <div className="circle_icon hover1" ref={usermenu} >
+        <div className={`circle_icon hover1 ${showUserMenu && "active_header"}`} ref={usermenu} >
         <div onClick={()=> {setShowUserMenu((prev) => !prev)}} > <ArrowDown1  /> </div>
             
             { showUserMenu && <UuserMenu user={user} />}
