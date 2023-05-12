c845ad9331d8d2c3a4b05e625b0d1e422d8235d9
abhiinavgupta <121946359+abhiinavgupta@users.noreply.github.com>
2023-05-12 10:37:25 +0530

    home responsiveness
    responsiveness

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { register, activateAccount, login } = require("../controllers/user")
+const { register, activateAccount, login, auth } = require("../controllers/user")
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/activate", activateAccount);
 router.post("/login", login);
+router.post("/auth", login);
 
 
 module.exports = router;
--- frontend/src/components/createPost/index.js
+++ frontend/src/components/createPost/index.js
@@ -1,10 +1,26 @@
+import { Feeling, LiveVideo, Photo } from "../../svg"
 import "./style.css"
 
-export default function CreatePost() {
+export default function CreatePost({user}) {
   return (
     <div className="createPost">
         <div className="createPost_header">
-
+        <img src={user?.picture} alt="" />
+        <div className="open_post">
+        Say hii to world, {user?.first_name}
+        </div>
+        </div>
+        <div className="create_splitter"></div>
+        <div className="createPost_body">
+        <div className="createPost_icon hover4">
+        <LiveVideo color="#f3425f" />  Video Chat
+        </div>
+        <div className="createPost_icon hover4">
+        <Photo color="#4bbf67" />  Photo/video
+        </div>
+        <div className="createPost_icon hover4">
+        <Feeling color="#f7b928" />  Feeling/Activity
+        </div>
         </div>
     </div>
   )
